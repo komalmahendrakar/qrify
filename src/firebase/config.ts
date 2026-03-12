dc752d824a01e79e17b74909492eb607b93ff50f
Shwetank Oli <shwetank.oli11@gmail.com>
2026-03-12 09:33:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7654933042-8d1d0",
+  "appId": "1:552771145117:web:f7dc08c40a62dc09d6f374",
+  "apiKey": "AIzaSyBb5VRNsxL9m_nO6Mzg_Smce5XVLZMs-mk",
+  "authDomain": "studio-7654933042-8d1d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552771145117"
+};
